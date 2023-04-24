b03585598e92eff356bc784c06ac61038e8771a7
yohanlimapo <yohan.limapo@gmail.com>
2023-04-24 08:35:54 -0300

    teste-basico-register-alurapic

---
+++ cypress/e2e/3-ALURAPIC/alurapic.spec.cy.js
@@ -0,0 +1,14 @@
+describe('login e regristro de usuários alura pic', () => {
+    beforeEach(() => {
+        cy.visit('https://alura-fotos.herokuapp.com/#/home')
+    })
+    it('verifica mensagens validacao', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+    })
+})
