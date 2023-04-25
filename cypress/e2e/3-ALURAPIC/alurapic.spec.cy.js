b9650f65ea8a500da6db510b2bd134c79eece21c
yohanlimapo <yohan.limapo@gmail.com>
2023-04-25 08:54:53 -0300

    testes regra de negocio individuais

--- cypress/e2e/3-ALURAPIC/alurapic.spec.cy.js
+++ cypress/e2e/3-ALURAPIC/alurapic.spec.cy.js
@@ -11,4 +11,36 @@ describe('login e regristro de usuários alura pic', () => {
         cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
         cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
     })
+
+    it('verifica mensagem email invalido', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="email"]').type('yohan');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+    })
+
+    it('verifica mensagem user name ser em letra minuscula', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="userName"]').type('YOHAN');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Must be lower case').should('be.visible');
+    })
+
+    it('verifica mensagem user name ter no minimo 2 (duas) letras', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="userName"]').type('Y');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 2').should('be.visible');
+    })
+
+    it('verifica mensagem senha minimo 8 caracteres', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="password"]').type('1234567');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+    })
 })
