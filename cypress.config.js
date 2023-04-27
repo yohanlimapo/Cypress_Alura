48942cf54e9afee39b32f3fca8f859d3e378a4a3
yohanlimapo <yohan.limapo@gmail.com>
2023-04-27 08:49:13 -0300

    integracao_cypress_cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "2np3nn",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
