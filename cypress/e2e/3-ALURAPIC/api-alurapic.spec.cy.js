86e0920c268d902b02b7907d97f9abff0086eb6c
yohanlimapo <yohan.limapo@gmail.com>
2023-05-01 16:39:47 -0300

    git ignore para dados sensíveis de login

--- cypress/e2e/3-ALURAPIC/api-alurapic.spec.cy.js
+++ cypress/e2e/3-ALURAPIC/api-alurapic.spec.cy.js
@@ -11,3 +11,18 @@ describe('Buscar fotos e dados', ()=>{
         })
     })
 })
+
+describe('fazer login do Flavio', ()=>{
+    it.only('fazer login', ()=>{
+        cy.request({
+            method:'POST',
+            url: 'https://apialurapic.herokuapp.com/user/login',
+            body: Cypress.env()
+        }).then ((res)=>{
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body).to.have.property('id')
+            expect(res.body.id).to.be.equal(1)
+        })
+    })
+})
